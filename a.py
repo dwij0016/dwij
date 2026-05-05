9f735431172441ceb3f8b6f1074fbc8d64cad6c7
unknown <dwijthakur0016@gmail.com>
2026-05-05 17:31:55 +0530

    file IO

--- a.py
+++ a.py
@@ -1,5 +1,19 @@
-a = 15
-d = 55
-c = a-d
+# a = 15
+# d = 55
+# c = a-d
 
-print(c)
+# print(c)
+
+f = open('poems.txt', 'r')
+d = f.read()
+print(d)
+f.close()
+
+g = open('greeting.txt' , 'w')
+g.write("Hello Good evening")
+g.close()
+
+
+g = open('greeting.txt' , 'a')
+g.write("\nHave a nice day")
+g.close()
