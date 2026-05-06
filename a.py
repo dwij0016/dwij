f8f1c5906a6c9f51934635a2d43e16099fc440d5
dwij0016 <dwijthakur0016@gmail.com>
2026-05-06 17:26:03 +0530

    with_statement

--- a.py
+++ a.py
@@ -17,3 +17,21 @@ g.close()
 g = open('greeting.txt' , 'a')
 g.write("\nHave a nice day")
 g.close()
+
+# syxtex = open('folder_name\\file_name' , 'mode of operation(like:- 'r' , 'w' , 'a')')  #folder_name(if any)#
+with open('poems.txt' , 'r') as r:
+    d = r.read()
+with open('new.txt' , 'w') as r:
+    d = r.write('hjjhfrsjhfrsdhfd')
+with open('new.txt' , 'a') as r:
+    d = r.write('\n87758996659')
+print(d)
+
+with open('poems.txt' , 'r') as s:
+    k = s.read()
+print(k)
+with open('copy_poems.txt' , 'w') as y:
+    g = y.write(k)
+print(y)
+
+
